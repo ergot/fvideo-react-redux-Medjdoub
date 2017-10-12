a4d5b1727dfd2c6a8c02f95e7cfbfc0080367902
ergot <wjose14@hotmail.com>
2017-10-12 12:05:03 +0200

    1-11

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,14 +7,15 @@ class Form extends React.Component {
         quantity: 0
     };
 
-
     render() {
         return (
             <div>
                 <h3>Ajouter des articles à acheter</h3>
                 <form >
-                    <input type="number" placeholder="quantité"/>
-                    <input type="text" placeholder="article"/>
+                    <input type="number" placeholder="quantité"
+                           value="" onChange={ (event)=>{this.setState({quantity:event.target.value})}}/>
+                    <input type="text" placeholder="article"
+                           value="" onChange={ (event)=>{this.setState({quantity:event.target.value})}}/>
                     <button type="submit">ajouter</button>
                 </form>
             </div>
