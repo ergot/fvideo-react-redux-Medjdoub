cae2c9671c99a7dba19464f5bc9914acfafd6be1
ergot <wjose14@hotmail.com>
2017-10-13 10:33:03 +0200

    1-15

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,16 @@ import Form from './Form'
 import ItemList from './ItemList'
 
 class App extends  React.Component {
+
+    addArticle (article){
+        console.log('article')
+    }
+
     render(){
         return(
             <div>
                 <h3>Liste de courses</h3>
-                <Form />
+                <Form  formTitle={'yolo'} addArticle={this.addArticle}/>
                 <ItemList />
             </div>
         )
--- src/components/Form.js
+++ src/components/Form.js
@@ -9,12 +9,13 @@ class Form extends React.Component {
 
     handleSubmit (event) {
         event.preventDefault()
+        this.props.addArticle(this.state)
     }
 
     render() {
         return (
             <div>
-                <h3>Ajouter des articles à acheter</h3>
+                <h3>this.props.title</h3>
                 <form onSubmit={this.handleSubmit}>
                     <input type="number" placeholder="quantité"
                            value={this.state.quantity} onChange={ (event)=>{this.setState({quantity:event.target.value})}}/>
