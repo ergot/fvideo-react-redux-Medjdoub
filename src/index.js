2bbad08fc6277fc3c7727aac77279d3dcbcdd983
ergot <wjose14@hotmail.com>
2017-10-14 12:00:40 +0200

    2-30

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {createStore, combineReducers } from 'redux'
 
 import App from './components/App'
+
+const store = createStore();
 
 ReactDom.render(<App />, document.getElementById('root'))
