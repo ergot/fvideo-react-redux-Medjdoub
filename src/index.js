cb56646dceb4104328095cd20ef04e3d9be62a7a
ergot <wjose14@hotmail.com>
2017-10-14 12:13:11 +0200

    2-31

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import {createStore, combineReducers } from 'redux'
 
 import App from './components/App'
 
-const store = createStore();
+
+
+const articlesReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_ARTICLE':
+            console.log('')
+            action.payload.id = Date.now();
+            return [...state, action.payload]
+        default:
+            return state;
+    }
+};
+
+
+const store = createStore(combineReducers({articles:articlesReducer}));
 
 ReactDom.render(<App />, document.getElementById('root'))
