0a25b23986339f9da4a428cfeafb49b0c65dde69
ergot <wjose14@hotmail.com>
2017-10-16 16:41:27 +0200

    2-44

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,46 @@
 import React from 'react'
 
-const Article = (props) => {
-    return (
-        <div>
-            {props.data.quantity} {props.data.name}
-        </div>
+class Article extends React.Component {
 
-    )
+    state = {
+        isInEditMode: false
+    }
+
+    toggleEditMode = () => {
+        this.setState({
+            isInEditMode: !this.state.isInEditMode
+        })
+    }
+
+    handleQuantityEdit = (event, article) => {
+        article.quantity = event.target.value
+        this.props.editArticle(article);
+    }
+
+    handleNameEdit = (event, article) => {
+        article.name = event.target.value
+        this.props.editArticle(article)
+    }
+
+    render () {
+        return (
+            <div>
+                <button className="btn" onClick={() => this.toggleEditMode()}>Modif</button>
+                {this.props.data.quantity} {this.props.data.name}
+                {
+                    this.state.isInEditMode?
+                        <span>
+                            <input type="number" value={this.props.data.quantity}
+                                   onChange={(event)=> {this.handleQuantityEdit(event, this.props.data)}}/>
+                            <input type="text" value={this.props.data.name}
+                                   onChange={(event)=> {this.handleNameEdit(event, this.props.data)}}/>
+                        </span> :
+                        <span>{this.props.data.quantity} {this.props.data.name}</span>
+                }
+            </div>
+        )
+    }
+
 }
 
 export default Article
